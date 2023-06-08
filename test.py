55fe1664c00f3b18e31a9ac70572d6e047252f58
jalmorGit <jalmor@yahoo.com>
2023-06-08 13:12:05 +0200

    Adivina num

--- test.py
+++ test.py
@@ -1,11 +1,12 @@
-# programa sacar numero aleatorio  
-# Autor: Alejandro Garcia   
-    
+# programa que adivina un número entre el 1 y el 10
 import random
 
-
-print (" Hola")
-
+numero = random.randint(1, 10)
+adivina = int(input("Adivina un número entre el 1 y el 10: "))
+if adivina == numero:
+     print("Has acertado")
+else:
+     print("Has fallado, el número era", numero)
 
 
 
