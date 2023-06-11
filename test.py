0c2b7ee6bfa45d10b43164fbe55cc2b3646df980
jalmorGit <jalmor@yahoo.com>
2023-06-12 00:14:04 +0200

    listas

--- test.py
+++ test.py
@@ -9,9 +9,17 @@ else:
      print("Has fallado, el número era", numero)
 
 
+ram_list = [4, 12, 8, 8]
+storage_list = [128, 128, 64, 128]
+price_list = [900, 899, 600, 1000]
 
+discounted_price_list = [x - (x*(5/100)) for x in price_list]
+print(discounted_price_list)
 
-
-
+# asking for customer's budget
+budget = int(input('Enter your budget(in dollars): '))
+# creating a list of Yes/No based on budget and discounted prices
+within_budget = ['Yes' if x <= budget else 'No' for x in discounted_price_list]
+print(within_budget)
 
                                     
